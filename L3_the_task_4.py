96ccb88f1a57e73c372a25e35dc7652f4dd6c7fa
Veretehin <dveretekhin@bk.ru>
2021-01-05 14:42:04 +0300

    delete

--- L3_the_task_4.py
+++
@@ -1,5 +0,0 @@
-def my_func(x, y):
-    return 1 if y == 0 else my_func(x, y + 1) * 1 / x
-
-
-print(round(my_func(input('Введите целое положительное число: '), input('Введите целое отрицательное число: ')), 10))
--- L3_the_task_5.py
+++
@@ -1,8 +0,0 @@
-num = 0
-try:
-    while True:
-        for i in map(int, input('Введите числа через пробел. Для выхода введите любуой текст или букву: ').split()):
-            num += i
-        print(num)
-except ValueError:
-    print(num)
